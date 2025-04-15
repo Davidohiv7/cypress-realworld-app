2bf7da330f11c14463f89417fe728af87e75b221
Davidohiv7 <davidbu7@hotmail.com>
2025-04-14 19:48:07 -0500

    feature(5): Add roll up visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 import istanbul from "vite-plugin-istanbul";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE");
@@ -26,6 +27,7 @@ export default defineConfig(({ command, mode }) => {
         exclude: ["node_modules", "cypress", "dist"],
         forceBuildInstrument: true,
       }),
+      visualizer({ open: true }),
     ],
     // to get aws amplify to work with vite
     resolve: {
